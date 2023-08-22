9e848a7337e37ac2c64b84b431639d3a99d9a6c4
pranaygaikwad1432@gmail.com <pranaygaikwad1432@gmail.com>
2023-08-22 18:02:20 +0530

    Required configurations added

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+
+const port = 5000 || process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// Static Files
+app.use(express.static("public"));
+
+//Templating Engine
+app.use(expressLayouts);
+app.set("layout", "./layout/main");
+app.set("view engine", "ejs");
+
+//Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`App listening on Port: ${port}`);
+});
