5b6ba529e971440515c18f7de1c0580de4fc1aab
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2023-08-24 17:31:53 +0530

    session created

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
-
 const expressLayouts = require("express-ejs-layouts");
-
 const connectDB = require("./server/config/db");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStrore = require("connect-mongo");
 
 const app = express();
-
 const port = 5000 || process.env.PORT;
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.urlencoded({ extended: true }));
 
