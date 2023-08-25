b4d63e4f64678d7e60f21d34db683f5be6402c3e
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2023-08-25 11:22:24 +0530

    check Authentication feature added

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { isLoggedIn } = require("../middleware/checkAuth");
 
 const dashboardController = require("../controllers/dashboardConroller");
 
@@ -8,6 +9,6 @@ Get Dashboard Page
 
 */
 
-router.get("/dashboard", dashboardController.dashboard);
+router.get("/dashboard", isLoggedIn, dashboardController.dashboard);
 
 module.exports = router;
