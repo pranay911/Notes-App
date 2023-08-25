8d4e585b949b5cc6835d65930d700f31437ac2db
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2023-08-25 12:06:34 +0530

    notes added

--- server/controllers/dashboardConroller.js
+++ server/controllers/dashboardConroller.js
@@ -9,9 +9,18 @@ exports.dashboard = async (req, res) => {
     title: "Dashboard",
     description: "Notes taking app in Node js",
   };
-  res.render("dashboard/index", {
-    userName: req.user.displayName,
-    locals,
-    layout: "../views/layouts/dashboard",
-  });
+
+  try {
+    /// get notes
+    const notes = await Note.find({});
+
+    res.render("dashboard/index", {
+      userName: req.user.firstName,
+      locals,
+      notes,
+      layout: "../views/layouts/dashboard",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
